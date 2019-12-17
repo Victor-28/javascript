048df7b4a2e6c157da164d742dd569337f1752f9
Victor-28 <victorakintola@gmail.com>
2019-12-17 11:06:05 +0100

    still working to make it give me correct answer, now 75% done

--- 02-maths/01-calculator-one/script.js
+++ 02-maths/01-calculator-one/script.js
@@ -9,22 +9,39 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function(){
+var a,b ;
+
+
+    a = document.getElementById("op-one").value;
+    b =document.getElementById("op-two").value;
     // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("addition").addEventListener("click", function() {
+       var Result= Number(a)+Number(b);
+        alert("The sum is equal to"+ Result);
         // perform an addition
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
+        var Result= a-b;
+        alert("The sum is equal to" + Result);
         // perform an substraction
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
+        var Result= a*b;
+        alert("The sum is equal to" + Result);
         // perform an multiplication
     });
 
     document.getElementById("division").addEventListener("click", function() {
+       var Result =a/b;
+        alert("the sum is equal to" + Result);
         // perform an division
     });
+
+
+
+
 })();
