5dbe3fe273baf81598ac368a52201a709efc3d70
Victor-28 <victorakintola@gmail.com>
2019-12-18 10:09:55 +0100

    ascending order working

--- 02-maths/04-sort-numbers/script.js
+++ 02-maths/04-sort-numbers/script.js
@@ -9,14 +9,22 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+
 
     // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("run").addEventListener("click", function() {
 
+        var points = ["2","4","14","10","90","23","16"];
+        alert("this is my array " + points);
+           var number = points.sort(function(a, b){return a-b});
+            alert("this is my array " + number);
+
+
+
+
         // your code here
 
     });
 
-})();
+
