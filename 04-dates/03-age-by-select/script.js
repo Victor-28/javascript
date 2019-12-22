ebd9231ae4e2ac7ea6f9680f36a59b5282a3d43e
Victor-28 <victorakintola@gmail.com>
2019-12-22 13:13:39 +0100

    the scary one

--- 04-dates/03-age-by-select/script.js
+++ 04-dates/03-age-by-select/script.js
@@ -9,8 +9,26 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+
 
     // your code here
 
-})();
+
+document.getElementById("run").addEventListener("click", function() {
+    //Stored values of input in variables
+    var birthDay = parseInt(document.getElementById("dob-day").value);
+    var birthMonth = parseInt(document.getElementById("dob-month").value);
+    var birthYear = (parseInt(document.getElementById("dob-year").value) - 1);
+    var currentDate = new Date();
+    //Calculating age
+    var age = (currentDate.getFullYear() - birthYear);
+    var month = (currentDate.getMonth() + 1);
+
+    if ( month - birthMonth  ||  month == birthMonth && currentDate.getDate() < birthDay) {
+        age--;
+    }
+    alert("Your age is " + age);
+})
+
+
+// this 4.3 was really scary so i asked help of other colleges,
