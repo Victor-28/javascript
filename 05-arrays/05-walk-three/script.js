487640a8779cddd6b7d4fd7d3d45a15e326f3fe4
Victor-28 <victorakintola@gmail.com>
2019-12-23 09:09:08 +0100

    having all the names in an array

--- 05-arrays/05-walk-three/script.js
+++ 05-arrays/05-walk-three/script.js
@@ -9,36 +9,47 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(
+    document.getElementById("run").addEventListener("click", function () {
 
-    var people = [
-        {
-            firstname: "Garald",
-            lastname: "Freschi",
-            email: "gfreschi0@discuz.net",
-        },
-        {
-            firstname: "Bendicty",
-            lastname: "Foster-Smith",
-            email: "bfostersmith1@mysql.com",
-        },
-        {
-            firstname: "Michelle",
-            lastname: "Draude",
-            email: "mdraude2@csmonitor.com",
-        },
-        {
-            firstname: "Cathee",
-            lastname: "Meineck",
-            email: "cmeineck3@t-online.de",
-        },
-        {
-            firstname: "Marianna",
-            lastname: "Coal",
-            email: "mcoal4@creativecommons.org",
-        },
-    ];
+        var people = [
+            {
+                firstname: "Garald",
+                lastname: "Freschi",
+                email: "gfreschi0@discuz.net",
+            },
+            {
+                firstname: "Bendicty",
+                lastname: "Foster-Smith",
+                email: "bfostersmith1@mysql.com",
+            },
+            {
+                firstname: "Michelle",
+                lastname: "Draude",
+                email: "mdraude2@csmonitor.com",
+            },
+            {
+                firstname: "Cathee",
+                lastname: "Meineck",
+                email: "cmeineck3@t-online.de",
+            },
+            {
+                firstname: "Marianna",
+                lastname: "Coal",
+                email: "mcoal4@creativecommons.org",
+            },
+        ];
+
+        for(var i = 0; i < people.length; i++) {
+            console.log(people[i].firstname);
+        }
+
+    }));
+
+
+
+
+// declaring the variable and using of reach
 
     // your code here
 
-})();
