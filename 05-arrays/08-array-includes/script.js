191781402cfde086dc133b8dd26877ae758c9a56
Victor-28 <victorakintola@gmail.com>
2019-12-23 10:36:06 +0100

    yes we have apple

--- 05-arrays/08-array-includes/script.js
+++ 05-arrays/08-array-includes/script.js
@@ -8,8 +8,9 @@
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
+document.getElementById("run").addEventListener("click", function () {
 
-(function() {
+
 
     var fruits = [
         "apple",
@@ -28,4 +29,9 @@
 
     // your code here
 
-})();
+
+if (fruits.includes("apple") == true)
+    console.log("We found the apple.");
+else
+    console.log("We found no apple. It is such a shame.");
+});
