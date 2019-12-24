0d5e41ba5478ea7fe2a9c10a2f5a41a42daac193
Victor-28 <victorakintola@gmail.com>
2019-12-24 10:51:09 +0100

    using innerHTML to change target to owned

--- 06-dom/01-select-one/script.js
+++ 06-dom/01-select-one/script.js
@@ -9,8 +9,12 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-
+(function (){
+    document.getElementById("target").innerHTML ="owned";
     // your code here
 
 })();
+
+
+// innerHTML to change target to owned
+
