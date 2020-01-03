d3a5bfdd38ea3099bcaf2788eb72ba15ea7da3a6
Victor-28 <victorakintola@gmail.com>
2020-01-03 10:42:04 +0100

    type writing effect

--- 07-misc/02-typewriter-effect/script.js
+++ 07-misc/02-typewriter-effect/script.js
@@ -9,26 +9,33 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-var i=0;
-var text = document.getElementById("target").innerHTML;
-console.log(text);
 
-function typing () {
-    if (i <text.length)
-    {
-        document.getElementById("material").innerHTML += text.charAt(i);
+
+(function() {
+
+    var i = 0;
+    var container = document.getElementById("target");
+    var text = document.getElementById("target").innerHTML;
+
+    var splitText= text.split("");
+    container.innerHTML="";
+    var max = 5;
+    var min= 1;
+    var random = Math.random();
+// var random = Math.floor(Math.random() * (max - min + 1) + min);
+
+    function typewriter() {
+        container.innerHTML  += splitText[i];
         i++;
-        SetTimeout(typing,70);
+
+        if (i < splitText.length){
+
+            setTimeout(typewriter, random * 100);
+        }
 
     }
-}
-
-typing();
-
-
-
-
-
+    typewriter();
+})();
 
 
 
