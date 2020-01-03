1de891982c0dbc54f4a4629aff44446f1aaa957d
Victor-28 <victorakintola@gmail.com>
2020-01-03 16:46:58 +0100

    worst user interface

--- 07-misc/04-worst-ui-two/script.js
+++ 07-misc/04-worst-ui-two/script.js
@@ -11,47 +11,63 @@
 
 // your code here
 var firstButton=460;
-document.getElementById("slider").addEventListener("change", function () {
+document.getElementById("part-one").addEventListener("click", function () {
     document.getElementById("target").innerHTML = "";
     document.getElementById("part-one").innerHTML = "";
     firstButton++;
     if (firstButton>499){
         firstButton=460;
     }
+    document.getElementById("part-one").innerHTML = firstButton;
+    document.getElementById("target").innerHTML = "+0" + firstButton + secondButton + thirdButton  + fourthButton;
 });
-----------------------------------------------------------------------------------------------------------
+
+    var secondButton=0;
+document.getElementById("part-two").addEventListener("click", function () {
+
+    document.getElementById("target").innerHTML = "";
+    document.getElementById("part-two").innerHTML = "";
+
+    secondButton++;
+    if (secondButton > 99){
+        secondButton = 00;
+    }
+    if (secondButton > 10){
+        secondButton = "0" + secondButton;
+    }
+    document.getElementById("part-two").innerHTML = secondButton;
+    document.getElementById("target").innerHTML = "+0" + firstButton + secondButton + thirdButton + fourthButton
+
+});
+
+    var thirdButton=0;
+document.getElementById("part-three").addEventListener("click", function () {
+
+    thirdButton++;
+     if (thirdButton > 99){
+         thirdButton ="00" + thirdButton;
+     }
+    if (thirdButton > 10){
+        thirdButton = "0" + thirdButton;
+        }
+    document.getElementById("part-three").innerHTML = secondButton;
+    document.getElementById("target").innerHTML = "+0" + firstButton + secondButton + thirdButton + fourthButton
+});
+
+    var fourthButton=0;
+document.getElementById("part-four").addEventListener("click", function () {
+
+        fourthButton++;
+       if(fourthButton > 99){
+            fourthButton="00" + fourthButton;
+       }
+        if(fourthButton > 10){
+            fourthButton ="0" + fourthButton;
+        }
+        document.getElementById("part-four").innerHTML= fourthButton;
+        document.getElementById("target").innerHTML = "0" + firstButton + secondButton + thirdButton + fourthButton
+
+});
 
 
-
-
-
-
-
-
-
-
-    var t=00
-document.getElementById("slider").addEventListener("change", function () {
-
-    t++;
-    if (x>99)
-        t = 00;
-};
-
-    var y=00
-document.getElementById("slider").addEventListener("change", function () {
-
-        y++;
-     if (x>99)
-         y=00;
-};
-
-    var z=00
-document.getElementById("slider").addEventListener("change", function () {
-
-        z++;
-       if(z>99)
-        z=00;
-};
-
-
+// tough task with help..i made each button a var:
